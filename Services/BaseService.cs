5ba956e3f00b4d212446ab17b0d71523314d2309
cypher4859 <charlesskayser@gmail.com>
2019-09-29 23:36:46 -0400

    The base service now chooses which environment its in

--- Services/BaseService.cs
+++ Services/BaseService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SMS_Notification_Client_v2_Interface.DatabaseConnectorModule;
+using SMS_Notification_Client_v2_Interface.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +11,28 @@ namespace SMS_Notification_Client_v2_Interface.Services
 {
     class BaseService
     {
+        public BaseService() {
+            // Make and use the DB Connector
+            // It will send the same DateSelection regardless.
+            string dbEnvironment = "alta";
+
+            if (dbEnvironment == "alta")
+            {
+                connector = new AltaPointDatabaseConnector();
+            }
+            else if (dbEnvironment == "total")
+            {
+                connector = new TotalMdDatabaseConnector();
+            }
+            else if (dbEnvironment == "Some other bullshit")
+            {
+
+            }
+        }
+
+        public BaseDatabaseConnector connector { get; set; }
+
+        //Create the list that will hold models
+        public IList<SmsNotificationDataResource> ListOfDataPackages { get; set; }
     }
 }
